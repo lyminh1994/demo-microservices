815beda175b82a749cf1de7bef29ec5ece1264b8
Ly Quang Minh <lyminh1994@gmail.com>
2024-09-15 23:00:33 +0700

    update application.yml and pom.xml

--- gateway-service/src/main/java/dev/hobie/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/dev/hobie/gateway_service/GatewayServiceApplication.java
@@ -1,34 +1,42 @@
 package dev.hobie.gateway_service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springdoc.core.models.GroupedOpenApi;
+import java.util.HashSet;
+import java.util.Set;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties.SwaggerUrl;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayServiceApplication.class, args);
+  }
 
-    @Bean
-    List<GroupedOpenApi> apis(RouteDefinitionLocator locator) {
-		var groups = new ArrayList<GroupedOpenApi>();
-		var definitions = locator.getRouteDefinitions().collectList().block();
-		assert definitions != null;
-		definitions.stream()
-				.filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
-				.forEach(
-						routeDefinition -> {
-							var name = routeDefinition.getId().replace("-service", "");
-							groups.add(
-									GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
-						});
-		return groups;
-	}
+  @Bean
+  @Lazy(false)
+  public Set<SwaggerUrl> apis(
+      RouteDefinitionLocator locator, SwaggerUiConfigParameters swaggerUiConfigParameters) {
+    Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
+    var definitions = locator.getRouteDefinitions().collectList().block();
+    assert definitions != null;
+    definitions.stream()
+        .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
+        .forEach(
+            routeDefinition -> {
+              var name = routeDefinition.getId().replace("-service", "");
+              var swaggerUrl =
+                  new AbstractSwaggerUiConfigProperties.SwaggerUrl(name, "/" + name, null);
+              urls.add(swaggerUrl);
+            });
+    swaggerUiConfigParameters.setUrls(urls);
+    return urls;
+  }
 }
