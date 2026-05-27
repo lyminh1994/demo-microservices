30f987b53f926898d48a1d6ae256044c4e105f84
Ly Quang Minh <lyminh1994@gmail.com>
2026-05-27 11:28:42 +0700

    Update JDK to 25-jre-alpine and adjust `main` method visibility in ConfigApplication

--- config-service/src/main/java/dev/hobie/config/ConfigApplication.java
+++ config-service/src/main/java/dev/hobie/config/ConfigApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
-	}
+  static void main(String[] args) {
+    SpringApplication.run(ConfigApplication.class, args);
+  }
 }
